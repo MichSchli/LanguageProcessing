ca0fd477cf2e13fbdb17c9343511e962a52de696
MichSchli <michael.sejr@gmail.com>
2015-03-18 15:14:22 +0100

    kgjhjkjh

--- Pipeline/Preprocessor.py
+++ Pipeline/Preprocessor.py
@@ -21,7 +21,7 @@ Testing playground:
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Preprocesses a file")
-    parser.add_argument("filepath", help="The path to the file to be preprocessed.")
+    parser.add_argument("file_path", help="The path to the file to be preprocessed.")
     args = parser.parse_args()
 
     print get_sentence_list(args.file_path)
